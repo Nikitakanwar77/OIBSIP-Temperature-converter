8a854f2dae0af9d451998f8b198c5e093469402e
Nikitakanwar77 <nikitakanwar441@gmail.com>
2024-10-06 19:37:19 +0530

    changed formulas

--- script.js
+++ script.js
@@ -22,5 +22,54 @@ function convert(){
             resultunit= 'C';
      }
      break;
+
+     case 'F':
+        if(tounit === 'C'){
+            converttedvalue=(temprature = 32)+5/9
+            resultunit = 'C';
+        }else if (tounit==='K') {
+               converttedvalue= (temprature = 32)+5/9 +273.15
+               resultunit= 'K';
+        }else if (tounit==='R') {
+            converttedvalue= temprature + 459.67
+            resultunit= 'R';
+      }else  {
+        converttedvalue= temprature;
+        resultunit= 'F';
+ } 
+ break;
+
+ case 'K':
+    if(tounit === 'C'){
+        converttedvalue=temprature - 273.15
+        resultunit = 'C';
+    }else if (tounit==='K') {
+           converttedvalue= (temprature )
+           resultunit= 'K';
+    }else if (tounit==='R') {
+        converttedvalue= temprature+273.15;
+        resultunit= 'R';
+  }else  {
+    converttedvalue= temprature;
+    resultunit= 'K';
+}
+break;
+
+case 'R':
+    if(tounit === 'C'){
+        converttedvalue=(temprature = 9/5)+32
+        resultunit = 'C';
+    }else if (tounit==='K') {
+           converttedvalue= temprature+273.15;
+           resultunit= 'K';
+    }else if (tounit==='R') {
+        converttedvalue= temprature+273.15;
+        resultunit= 'R';
+  }else  {
+    converttedvalue= temprature;
+    resultunit= 'R';
+}
+break;
+
     }
 }
